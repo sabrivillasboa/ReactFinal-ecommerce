eef094b4e280318639f344112abfe3348cdee2e5
Sabrina <sabrinavillasboa@gmail.com>
2023-11-09 19:52:57 -0300

    generando orden

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,74 +1,95 @@
-import React from "react";
-import { TextField, Box, Button} from "@mui/material";
+import { useContext } from "react";
+import { TextField, Box, Button } from "@mui/material";
 import { useState } from "react";
+import { serverTimestamp, updateDoc } from "firebase/firestore";
+import { CartContext } from "../../context/CartContext";
+import { database } from "../../firebaseConfig";
+import { collection, addDoc, doc } from "firebase/firestore";
 
 const Checkout = () => {
-    const [nombre, setNombre] = useState("");
-    const [apellido, setApellido] = useState("");
-    const [email, setEmail] = useState("");
+  const [userData, setUserData] = useState({
+    name: "",
+    surname: "",
+    phone: "",
+    email: "",
+    //repeatEmail:"",
+  });
 
-    const capturarNombre=(e) =>{
-        setNombre(e.target.value)
+  const [orderId, setOrderId] = useState(null);
+
+  const { cart, totalPrice, deleteCart } = useContext(CartContext);
+
+  const total = totalPrice();
+
+  const handleChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let order = {
+      buyer: userData,
+      items: cart,
+      total: total,
+      date: serverTimestamp(),
     };
+    let ordersCollections = collection(database, "orders");
+    addDoc(ordersCollections, order).then((res) => setOrderId(res.id));
 
-    const capturarApellido=(e) =>{
-        setApellido(e.target.value)
-    };
+    cart.forEach(element => {
+        let refDoc = doc (database, "dataBooks", element.id)
+        updateDoc(refDoc, {stock: element.stock - element.quantity,})
+    });
 
-    const capturarEmail=(e) =>{
-        setEmail(e.target.value)
-    };
-    //SE PUEDE REFACTORIZAR LOS ESTADOS Y LAS FUNCIONES A UNA SOLA PARA TODO.
-    //LA FUNCION QUE MANEJA EL ONCHANGE SE LLAMA HANDLECHANGE.
+    deleteCart();
+  };
 
-    const handleSubmit =(e) =>{
-        e.preventDefault()
-        console.log(nombre);
-        console.log(apellido);
-        console.log(email);
-        
-    }
-    
-    return (
-        <div style={{display:"flex", height:"90vh", flexDirection:"column"}}>
-        <h2>Checkout</h2>
-        <Box component="form"
-            onSubmit={handleSubmit}
-            >
+  return (
+    <>
+      {orderId ? (
+        <h2>Compra finalizada. Su nro de comprobante es {orderId}</h2>
+      ) : (
+        <div
+          style={{ display: "flex", height: "90vh", flexDirection: "column" }}
+        >
+          <h2>Checkout</h2>
+          <Box component="form" onSubmit={handleSubmit}>
             <TextField
-            id="nombre"
-            label="nombre"
-            type="text"
-            onChange={capturarNombre}
-            autoComplete="off"
-            variant="outlined"
-            value={nombre}
-            name="nombre"
-        />
-        <TextField
-            id="apellido"
-            label="apellido"
-            type="text"
-            onChange={capturarApellido}
-            autoComplete="off"
-            variant="outlined"
-            value={apellido}
-            name="apellido"
-        />
-        <TextField
-            id="email"
-            label="email"
-            type="email"
-            onChange={capturarEmail}
-            variant="outlined"
-            value={email}
-            name="email"
-        />
-        <Button variant= "contained" type="submit" >Enviar</Button>
-        </Box>
+              label="nombre"
+              type="text"
+              onChange={handleChange}
+              variant="outlined"
+              name="name"
+            />
+            <TextField
+              label="apellido"
+              type="text"
+              onChange={handleChange}
+              variant="outlined"
+              name="surname"
+            />
+            <TextField
+              label="telefono"
+              type="number"
+              onChange={handleChange}
+              variant="outlined"
+              name="phone"
+            />
+            <TextField
+              label="email"
+              type="email"
+              onChange={handleChange}
+              variant="outlined"
+              name="email"
+            />
+            <Button variant="contained" type="submit">
+              Enviar
+            </Button>
+          </Box>
         </div>
-    );
+      )}
+    </>
+  );
 };
 
 export default Checkout;
-
